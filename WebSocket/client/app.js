c37698c1224655f0da8e3004b6d2b74e41136302
Paras Agrawal <parasagrawal71@gmail.com>
2025-06-13 13:17:07 +0530

    Add typing indicator, connection welcome msg, and disconnection broadcast using socket.io and express

--- WebSocket/client/app.js
+++ WebSocket/client/app.js
@@ -1,8 +1,10 @@
 const socket = new io("ws://localhost:3001");
 
+const input = document.querySelector("input");
+const typingPara = document.querySelector("#typing");
+
 function sendMessage(e) {
   e.preventDefault();
-  const input = document.querySelector("input");
   if (input.value) {
     // socket.send(input.value);
     socket.emit("message", input.value);
@@ -24,4 +26,22 @@ socket.on("message", (data) => {
   const li = document.createElement("li");
   li.textContent = data;
   ul.appendChild(li);
+  typingPara.textContent = "";
+});
+
+input.addEventListener("keypress", () => {
+  // activity event
+  socket.emit("activity", socket.id.substring(0, 5));
+});
+
+let activityTimer;
+
+socket.on("activity", (name) => {
+  typingPara.textContent = `${name} is typing...`;
+
+  // clear after 3 seconds
+  if (activityTimer) clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    typingPara.textContent = "";
+  }, 3000);
 });
--- WebSocket/server/app.js
+++ WebSocket/server/app.js
@@ -1,9 +1,11 @@
-const { Server } = require("socket.io");
-const http = require("http");
+import { Server } from "socket.io";
+import express from "express";
 
-const httpServer = http.createServer();
-
-httpServer.listen(3001);
+const app = express();
+const PORT = process.env.PORT || 3001;
+const httpServer = app.listen(PORT, () => {
+  console.log(`Server is now listening to ${PORT}`);
+});
 
 const io = new Server(httpServer, {
   cors: {
@@ -14,11 +16,36 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log(`connection established with ID: ${socket.id}`);
 
+  // Upon connection - only to current user
+  socket.emit(
+    "message",
+    `Welcome to Chat App (User Id: ${socket.id.substring(0, 5)})`,
+  );
+
+  // Upon connection - to all other users
+  socket.broadcast.emit(
+    "message",
+    `User ${socket.id.substring(0, 5)} connected`,
+  );
+
+  // capturing the activity event
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name);
+  });
+
   socket.on("message", (data) => {
     // console.log("message received: ", Buffer.from(data).toString());
     // socket.send(`${data}`);
 
     console.log("message received: ", data);
     io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
+  });
+
+  socket.on("disconnect", () => {
+    // Upon disconnection - to all other users
+    socket.broadcast.emit(
+      "message",
+      `User ${socket.id.substring(0, 5)} disconnected`,
+    );
   });
 });
